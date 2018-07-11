f9ed86f9a616367cf1ea200ae5bae8f3095c8306
gulu210 <513745021@qq.com>
2018-07-11 23:18:00 +0800

    新建首页

--- GuangFish/Classes/HomeViewController.h
+++ GuangFish/Classes/HomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HomeViewController.h
 //  GuangFish
 //
 //  Created by 顾越超 on 2018/7/11.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HomeViewController : UICollectionViewController
 
 @end
-
